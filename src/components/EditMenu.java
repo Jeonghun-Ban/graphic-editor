b59fb72cca914c496609103cc786903cf78bf871
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-03-08 14:40:15 +0900

    feat: Add menu items to menu components

--- src/components/EditMenu.java
+++ src/components/EditMenu.java
@@ -5,5 +5,11 @@ import javax.swing.*;
 public class EditMenu extends JMenu {
     public EditMenu() {
         super("Edit");
+
+        add(new JMenuItem("Cut"));
+        add(new JMenuItem("Copy"));
+        add(new JMenuItem("Paste"));
+        add(new JMenuItem("Delete"));
+        add(new JMenuItem("Select All"));
     }
 }
--- src/components/FileMenu.java
+++ src/components/FileMenu.java
@@ -5,5 +5,11 @@ import javax.swing.*;
 public class FileMenu extends JMenu {
     public FileMenu() {
         super("File");
+
+        add(new JMenuItem("New"));
+        add(new JMenuItem("Open"));
+        add(new JMenuItem("Save"));
+        add(new JMenuItem("Save As..."));
+        add(new JMenuItem("Exit"));
     }
 }
