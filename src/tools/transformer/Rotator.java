f453f5814670740d68c7dd69eae583e756a8bebf
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-05-27 00:53:54 +0900

    feat: Add rotator as transformer

---
+++ src/tools/transformer/Rotator.java
@@ -0,0 +1,26 @@
+package tools.transformer;
+
+import java.awt.Graphics2D;
+import java.awt.Point;
+import tools.draw.DrawShape;
+
+public class Rotator extends Transformer{
+
+  private Point startPoint;
+
+  public Rotator(DrawShape drawShape) {
+    super(drawShape);
+    this.startPoint = new Point();
+  }
+
+  @Override
+  public void init(Point startPoint) {
+    this.startPoint = startPoint;
+  }
+
+  @Override
+  public void transform(Graphics2D g2D, Point currentPoint) {
+    drawShape.rotateTo(startPoint, currentPoint);
+    this.startPoint = currentPoint;
+  }
+}
